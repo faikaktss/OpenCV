7f9626f75c9ebe858ce4c07bf98742232012f3cd
𝐅𝐚𝐢𝐤 𝐀𝐤𝐭𝐚𝐬 <148656104+faikaktss@users.noreply.github.com>
2025-12-05 20:55:20 +0300

    ImageNumpy2

---
+++ Görüntü işleme/ImageNumpy2.py
@@ -0,0 +1,20 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import os
+
+
+# img = cv2.imread(os.path.join(os.path.dirname(os.path.abspath(__file__)),"./Ekran Resmi 2025-12-03 20.22.51.png"))
+
+# img_bgr = cv2.imread(os.path.join(os.path.dirname(os.path.abspath(__file__)),"./Ekran Resmi 2025-12-03 20.22.51.png"), cv2.IMREAD_COLOR)
+
+# plt.imshow(img_bgr)
+
+# print(img.shape)
+
+
+
+img = cv2.imread(os.path.join(os.path.dirname(os.path.abspath(__file__)),"./Ekran Resmi 2025-12-03 20.22.51.png"))
+cv2.imshow("Orijinal resim", img)
+
+cv2.waitKey()
