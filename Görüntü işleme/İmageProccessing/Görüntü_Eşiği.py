8cdd87771730b4476e28c37d72aa9e4ca7ed14e9
𝐅𝐚𝐢𝐤 𝐀𝐤𝐭𝐚𝐬 <148656104+faikaktss@users.noreply.github.com>
2025-12-06 19:16:18 +0300

    GörüntüEşiği

---
+++ Görüntü işleme/İmageProccessing/Görüntü_Eşiği.py
@@ -0,0 +1,10 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+img = cv2.imread('../Ekran Resmi 2025-12-03 20.22.51.png')
+plt.imshow(img)
+
+img = cv2.imread('../Ekran Resmi 2025-12-03 20.22.51.png')
+plt.imshow(img,cmap="gray")
