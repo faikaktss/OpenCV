7ff96f60d27978601264ca76da0636d85ffc8fd2
𝐅𝐚𝐢𝐤 𝐀𝐤𝐭𝐚𝐬 <148656104+faikaktss@users.noreply.github.com>
2025-12-08 19:20:29 +0300

    Canlı Kamerada Çizim

---
+++ Görüntü işleme/Video-Basic/VideodaÇizim.py
@@ -0,0 +1,22 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+x = width // 2
+y = height // 2
+
+w = width // 4
+h = height // 4
+
+while True:
+    ret ,frame = cap.read()
+    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    cv2.imshow('Kamera', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    cap.release()
+
+cv2.destroyAllWindows()
